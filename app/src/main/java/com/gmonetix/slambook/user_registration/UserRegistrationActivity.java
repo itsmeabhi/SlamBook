1e17f933771921201f2adb7bc52a8145d744143b
Gmonetix <gmonetix@gmail.com>
2017-02-03 11:51:26 -0800

    2nd commit

--- app/src/main/java/com/gmonetix/slambook/user_registration/UserRegistrationActivity.java
+++ app/src/main/java/com/gmonetix/slambook/user_registration/UserRegistrationActivity.java
@@ -1,17 +1,23 @@
 package com.gmonetix.slambook.user_registration;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.gmonetix.slambook.R;
 
 public class UserRegistrationActivity extends AppCompatActivity {
 
-    private de.hdodenhof.circleimageview.R circleimageview;
+    private ImageView circleimageview;
+    private static final int GALLERY_INTENT_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,22 @@ public class UserRegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       // circleimageview = findViewById(R.id.iv_profile_image);
+        circleimageview = (ImageView) findViewById(R.id.iv_profile_image);
+        circleimageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery_intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery_intent,GALLERY_INTENT_ACTIVITY);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_INTENT_ACTIVITY && resultCode == RESULT_OK && data != null){
+            Uri selected_image = data.getData();
+            circleimageview.setImageURI(selected_image);
+        }
     }
 }
