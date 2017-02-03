f8fc731a1eba25fda0f33826c2c15a8e821a94e2
Gmonetix <gmonetix@gmail.com>
2017-02-03 15:58:03 -0800

    2nd commit

---
+++ app/src/main/java/com/gmonetix/slambook/user_profile/UserProfile.java
@@ -0,0 +1,15 @@
+package com.gmonetix.slambook.user_profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.gmonetix.slambook.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
--- app/src/main/java/com/gmonetix/slambook/user_registration/UserRegistrationActivity.java
+++ app/src/main/java/com/gmonetix/slambook/user_registration/UserRegistrationActivity.java
@@ -1,23 +1,36 @@
 package com.gmonetix.slambook.user_registration;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.gmonetix.slambook.R;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
 public class UserRegistrationActivity extends AppCompatActivity {
 
     private ImageView circleimageview;
     private static final int GALLERY_INTENT_ACTIVITY = 1;
+    private String name, email, username, password, description, dob, phonenumber;
+    private EditText Name, Email, Username, Password, Description, Dob, PhoneNumber;
+    Button submit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +39,8 @@ public class UserRegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        circleimageview = (ImageView) findViewById(R.id.iv_profile_image);
+        init();
+
         circleimageview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +48,25 @@ public class UserRegistrationActivity extends AppCompatActivity {
                 startActivityForResult(gallery_intent,GALLERY_INTENT_ACTIVITY);
             }
         });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap profile = ((BitmapDrawable) circleimageview.getDrawable()).getBitmap();
+            }
+        });
+    }
+
+    private void init() {
+        Name = (EditText) findViewById(R.id.et_name);
+        Email = (EditText) findViewById(R.id.et_email);
+        Username = (EditText) findViewById(R.id.et_username);
+        Password = (EditText) findViewById(R.id.et_password);
+        Description = (EditText) findViewById(R.id.et_description);
+        Dob = (EditText) findViewById(R.id.et_dob);
+        PhoneNumber = (EditText) findViewById(R.id.et_phone_number);
+        submit = (Button) findViewById(R.id.register_btn);
+        circleimageview = (ImageView) findViewById(R.id.iv_profile_image);
     }
 
     @Override
@@ -42,6 +75,37 @@ public class UserRegistrationActivity extends AppCompatActivity {
         if (requestCode == GALLERY_INTENT_ACTIVITY && resultCode == RESULT_OK && data != null){
             Uri selected_image = data.getData();
             circleimageview.setImageURI(selected_image);
+        }
+    }
+
+    private class RegisterInBackground extends AsyncTask<Void,Void,Void>{
+
+        Bitmap image;
+        String profilePicName;
+
+        public RegisterInBackground(Bitmap image, String profilePicName)
+        {
+            this.image = image;
+            this.profilePicName = profilePicName;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // 100 is the best quality
+            image.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+            String encodedImage = Base64.encodeToString(outputStream.toByteArray(),Base64.DEFAULT);
+
+            // org.Apache.http
+         //   ArrayList<NameValuePair> dataToSend = new ArrayList<>();
+       //     dataToSend.add(new Bas);
+
+            return null;
         }
     }
 }
